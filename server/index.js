9a36d066a857cc4f428592fb0ddd28a247a9ff17
Muzaffarmoti <muzaffarmoti@gmail.com>
2024-09-29 18:58:22 +0530

    to resolve the error of message

--- server/index.js
+++ server/index.js
@@ -30,7 +30,8 @@ const server = app.listen(process.env.PORT,()=>{
 
 const io = socket(server,{
     cors:{
-        origin: "http://localhost:3000",
+        // origin: "http://localhost:3000",
+        origin: "https://chit-chat-21.netlify.app/",
         credentials: true,
     },
 });
